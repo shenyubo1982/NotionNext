f7221fdc7f279f5aeb748a90e43ae2b3b7532c4a
shenyubo <shenyubo_1024@163.com>
2023-11-15 17:34:03 +0800

    config:modify website info

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'ManMenDao', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '从一个人开始，向一群人出发。ManMen DAO由投研、技术、运营等领域的Web3实干家发起，以DAO的方式组织内容输出和产品实践，以稳健步伐，共赴硅基未来。🚀', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://dao.mifuts.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.mendao.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'web3,cryptoCoin,vc', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
